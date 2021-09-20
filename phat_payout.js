d160ff1fc89964b476cc8bfa186eae255b022d24
CosmiCloud <tybranha@gmail.com>
2021-09-20 18:57:38 -0400

    Origintrail Manual Payouts

---
+++ phat_payout.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+try{
+
+  async function payout(){
+    var jerbs = "sudo curl -X GET https://v5api.othub.info/api/nodes/DataHolder/"+process.env.NODE_ID+"/jobs"
+    var jerbs = await exec(jerbs);
+    var jerbs = JSON.parse(jerbs.stdout);
+
+    for(var i = 0; i < (jerbs.length); i++) {
+      if(jerbs[i].BlockchainID == "1" && process.env.ETHEREUM == 'YES'){
+        var offer_id = jerbs[i].OfferId
+        var payout_com = "curl -s -X GET http://127.0.0.1:8900/api/latest/payout?offer_id="+offer_id
+        await exec(payout_com);
+        console.log('\x1b[32m','Triggered eth payout for offer '+jerbs[i].OfferId);
+
+      }else if(jerbs[i].BlockchainID == "2" && process.env.XDAI == 'YES'){
+        var offer_id = jerbs[i].OfferId
+        var payout_com = "curl -s -X GET http://127.0.0.1:8900/api/latest/payout?offer_id="+offer_id
+        await exec(payout_com);
+        console.log('\x1b[32m','Triggered xdai payout for offer '+jerbs[i].OfferId);
+
+      }else if(jerbs[i].BlockchainID == "3" && process.env.POLYGON == 'YES'){
+        var offer_id = jerbs[i].OfferId
+        var payout_com = "curl -s -X GET http://127.0.0.1:8900/api/latest/payout?offer_id="+offer_id
+        await exec(payout_com);
+        console.log('\x1b[32m','Triggered polygon payout for offer '+jerbs[i].OfferId);
+
+      }else{
+        console.log('\x1b[35m','Skipped payout for offer '+jerbs[i].OfferId)
+      }
+    }
+
+  }
+  payout();
+}catch(e){
+  console.log(e);
+}
