817259ef266798b1590f2682878391ea5ec8f1bb
CosmiCloud <tybranha@gmail.com>
2021-09-20 21:03:06 -0400

    again

--- phat_payout.js
+++ phat_payout.js
@@ -10,21 +10,22 @@ try{
     var jerbs = JSON.parse(jerbs.stdout);
 
     for(var i = 0; i < (jerbs.length); i++) {
-      if(jerbs[i].BlockchainID == "1" && process.env.ETHEREUM == 'YES' && jerbs[i].Paidout == "false"){
+      console.log(jerbs[i].Paidout)
+      if(jerbs[i].BlockchainID == "1" && process.env.ETHEREUM == 'YES' && jerbs[i].Paidout == false){
         var offer_id = jerbs[i].OfferId
         var payout_com = "curl -s -X GET http://127.0.0.1:8900/api/latest/payout?offer_id="+offer_id
         var result = await exec(payout_com);
         console.log('\x1b[32m','Triggered eth payout for offer '+jerbs[i].OfferId);
         console.log('\x1b[33m',result.stdout);
 
-      }else if(jerbs[i].BlockchainID == "2" && process.env.XDAI == 'YES' && jerbs[i].Paidout == "false"){
+      }else if(jerbs[i].BlockchainID == "2" && process.env.XDAI == 'YES' && jerbs[i].Paidout == false){
         var offer_id = jerbs[i].OfferId
         var payout_com = "curl -s -X GET http://127.0.0.1:8900/api/latest/payout?offer_id="+offer_id
         var result = await exec(payout_com);
         console.log('\x1b[32m','Triggered xdai payout for offer '+jerbs[i].OfferId);
         console.log('\x1b[33m',result.stdout);
 
-      }else if(jerbs[i].BlockchainID == "3" && process.env.POLYGON == 'YES' && jerbs[i].Paidout == "false"){
+      }else if(jerbs[i].BlockchainID == "3" && process.env.POLYGON == 'YES' && jerbs[i].Paidout == false){
         var offer_id = jerbs[i].OfferId
         var payout_com = "curl -s -X GET http://127.0.0.1:8900/api/latest/payout?offer_id="+offer_id
         var result = await exec(payout_com);
